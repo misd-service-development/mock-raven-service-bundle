db8acdbdfc3d3f4a4b6d5d64c16b36ec3d68be7f
thewilkybarkid <chriswilkinson84@gmail.com>
2013-04-08 09:21:24 +0100

    Add WLS response for the other status codes

---
+++ WlsResponse/GeneralRequestParameterErrorWlsResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the MisdMockRavenServiceBundle for Symfony2.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Misd\MockRavenServiceBundle\WlsResponse;
+
+class GeneralRequestParameterErrorWlsResponse extends AbstractWlsResponse
+{
+    protected function getStatus()
+    {
+        return 530;
+    }
+}
